7e49e9389dc289e9038941a795329df4e72114a8
Tilesh Bhaga <tilesh_bhaga@c02nl6g0f8j4.tbwa.co.za>
2019-11-18 13:04:06 +0200

    test broken

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,20 +1,22 @@
-// import React, { Component } from 'react';
+import React, { Component } from "react";
 
-// class Gallery extends Component {
-//     state = { images:[] }
+class Gallery extends Component {
+  state = { images: [] };
 
-//     importAll(r) {
-//         return r.keys().map(r);
-//       }
+  importAll(r) {
+    return r.keys().map(r);
+  }
 
-//     componentDidMount(){
-//         images.setState(importAll(require.context('./gallery/', false, /\.(png|jpe?g|svg)$/)));
-//     }
+  componentDidMount() {
+    images.setState(
+      importAll(require.context("./gallery/", false, /\.(png|jpe?g|svg)$/))
+    );
+  }
 
-//     render() {
-//         console.log(this.state.images)
-//         // return (  );
-//     }
-// }
+  render() {
+    console.log(this.state.images);
+    // return (  );
+  }
+}
 
-// export default Gallery;
+export default Gallery;
