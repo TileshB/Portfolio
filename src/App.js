f8b593810d32180f4803d530467381306d7e2d68
TileshB <tileshb@gmail.com>
2020-08-07 15:04:01 +0200

    added smart naming for on tab

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import "./App.scss";
 import FullHeight from "./components/full";
 
 function App() {
+  var vis = (function(){
+    var stateKey, eventKey, keys = {
+        hidden: "visibilitychange",
+        webkitHidden: "webkitvisibilitychange",
+        mozHidden: "mozvisibilitychange",
+        msHidden: "msvisibilitychange"
+    };
+    for (stateKey in keys) {
+        if (stateKey in document) {
+            eventKey = keys[stateKey];
+            break;
+        }
+    }
+    return function(c) {
+        if (c) document.addEventListener(eventKey, c);
+        return !document[stateKey];
+    }
+})();
+
+var visible = vis(); // gives current state
+
+vis(function(){
+  document.title = vis() ? 'Welcome Stranger' : 'Dont leave!';
+}); 
+
   return (
     <div className="App">
       <FullHeight />
